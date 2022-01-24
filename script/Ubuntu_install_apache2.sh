ae07302ea0dfe6bfce8cd2f72f0e8c11eafc212e
xiehuangjun <60767398+xiehuangjun@users.noreply.github.com>
2022-01-24 08:21:51 +0800

    Update Ubuntu_install_apache2.sh

--- script/Ubuntu_install_apache2.sh
+++ script/Ubuntu_install_apache2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt-get install apache2 -y
+sudo apt-get update -y
+sudo apt-get install git
+cd /
+sudo git clone https://github.com/xiehuangjun/terraform.git
+cd /terraform/script
+sudo su
+sudo echo "Apache2 Server Status is okay" > /var/www/html/health.html
