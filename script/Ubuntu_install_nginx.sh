7f9265521af2a525329046693163c9c0b5827cbf
xiehuangjun <60767398+xiehuangjun@users.noreply.github.com>
2022-01-24 10:47:49 +0800

    Update Ubuntu_install_nginx.sh

--- script/Ubuntu_install_nginx.sh
+++ script/Ubuntu_install_nginx.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install git
-cd /
-sudo git clone https://github.com/xiehuangjun/terraform.git
-cd /terraform/script
+sudo apt-get install nginx
 sudo su
 sudo echo "Nginx Server Status is ok" > /var/www/html/health.html
